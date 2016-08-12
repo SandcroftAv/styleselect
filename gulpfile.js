3cdf211bc304b9e0c9eaf9a8fa6758f9e2eaed7e
Javier Soler <gasch05@gmail.com>
2016-08-12 11:55:27 +0100

    Update/clean up packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,28 @@
 // Run 'gulp' to do the important stuff
 var gulp = require('gulp'),
-  prefixer = require('gulp-autoprefixer'),
-  sass = require('gulp-sass'),
-  livereload = require('gulp-livereload'),
-  nodemon = require('gulp-nodemon'),
-  jshint = require('gulp-jshint'),
-  connect = require('gulp-connect');
+    prefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect');
 
 var path = require('path');
 
 gulp.task('sass', function () {
   gulp
-    .src('./scss/styleselect.scss')
-    .pipe(sass({
-      paths: ['scss']
-    }))
-    .pipe(prefixer('last 2 versions', 'ie 9'))
-    .pipe(gulp.dest('./css'))
-    .pipe( connect.reload() );
+      .src('./scss/styleselect.scss')
+      .pipe(sass({
+        paths: ['scss']
+      }))
+      .pipe(prefixer('last 5 versions', 'ie 9'))
+      .pipe(gulp.dest('./css'))
+      .pipe( connect.reload() );
   gulp
-    .src('./scss/main.scss')
-    .pipe(sass({
-      paths: ['scss']
-    }))
-    .pipe(prefixer('last 2 versions', 'ie 9'))
-    .pipe(gulp.dest('./css'))
-    .pipe( connect.reload() );
+      .src('./scss/main.scss')
+      .pipe(sass({
+        paths: ['scss']
+      }))
+      .pipe(prefixer('last 5 versions', 'ie 9'))
+      .pipe(gulp.dest('./css'))
+      .pipe( connect.reload() );
 });
 
 // The default task (called when you run `gulp`)
