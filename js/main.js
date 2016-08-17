fe352cc708f261880a7e60e576c2f9b7923ba265
Javier Soler <gasch05@gmail.com>
2016-08-17 12:56:20 +0100

    Support for styling multiple select boxes with the same css selector

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-define(['styleselect'], function(styleSelect){
-  var log = console.log.bind(console);
+define(['styleselect'], function (styleSelect) {
+    var log = console.log.bind(console);
 
-  styleSelect('select');
+    styleSelect('select');
 
-  // Rest of code is for demonstration purposes...
-  var selectBox = document.querySelector('select')
+    // Rest of code is for demonstration purposes...
+    var selectBoxes = document.querySelectorAll('select');
 
-  var demoBinding = new Ractive({
-    el: document.querySelector('p.result'),
-    template: 'Value of real select box is currently {{ value }}',
-    data: {value: selectBox.value}
-  })
+    if (selectBoxes.length) {
+        selectBoxes.forEach(function (selectBox, index) {
+            var demoBinding = new Ractive({
+                el: document.querySelectorAll('p.result')[index],
+                template: 'Value of real select box is currently {{ value }}',
+                data: {value: selectBox.value}
+            });
 
-  selectBox.addEventListener('change', function(){
-    demoBinding.set('value', selectBox.value)
-  })
-})
+            selectBox.addEventListener('change', function () {
+                demoBinding.set('value', selectBox.value)
+            });
+        });
+    }
+});
--- js/styleselect.js
+++ js/styleselect.js
@@ -148,234 +148,245 @@
 		})
 	};
 
+	// Detect iOS/Android
+	var isTouch = navigator.userAgent.match( /iPad|iPhone|Android/i );
+
+	// Close all selects except the one passed
+	var closeAllStyleSelects = function(exception){
+		queryAll('.style-select').forEach(function(styleSelectEl) {
+			if ( styleSelectEl !== exception ) {
+				styleSelectEl.classList.remove('open');
+			}
+		});
+	};
+
+	// Toggle open/closed states for non-touch
+	var toggleStyledSelect = function(styledSelectBox){
+		if ( ! isTouch ) {
+			if ( ! styledSelectBox.classList.contains('open') ) {
+				// If we're closed and about to open, close other style selects on the page
+				closeAllStyleSelects(styledSelectBox);
+			}
+			// Then toggle open/close
+			styledSelectBox.classList.toggle('open');
+		}
+	};
+
+	// Update styled selected options and real select box
+	var changeRealSelectBox = function(realSelect, styledSelect, newValue, newLabel) {
+
+		var styleSelectOptions = styledSelect.querySelectorAll('.ss-option');
+		var selectedOption = styledSelect.querySelector('.ss-selected-option');
+
+		// Close styledSelect
+		styledSelect.classList.remove('open');
+
+		// Update styled value
+		selectedOption.textContent = newLabel;
+		selectedOption.dataset.value = newValue;
+
+		// Update the 'tick' that shows the option with the current value
+		styleSelectOptions.forEach(function(styleSelectOption){
+			if ( styleSelectOption.dataset.value === newValue) {
+				styleSelectOption.classList.add('ticked')
+			} else {
+				styleSelectOption.classList.remove('ticked')
+			}
+		});
+
+		// Update real select box if not touch only
+		if ( ! isTouch ) {
+			realSelect.value = newValue;
+
+			// Send 'change' event to real select - to trigger any change event listeners
+			var changeEvent = new CustomEvent('change');
+			realSelect.dispatchEvent(changeEvent);
+		}
+	};
 
 	// The 'styleSelect' main function
 	// selector:String - CSS selector for the select box to style
 	return function(selector) {
 
-		// Detect iOS/Android
-		var isTouch = navigator.userAgent.match( /iPad|iPhone|Android/i );
+		var realSelects = queryAll(selector);
 
-		var realSelect = query(selector),
-			realOptions = realSelect.children,
-			selectedIndex = realSelect.selectedIndex,
-			uuid = makeUUID(),
-			className = 'style-select' + (isTouch ? ' touch' : ''),
-			styleSelectHTML = '<div class="' + className + '" aria-hidden="true" data-ss-uuid="' + uuid + '">';
-
-		// The index of the item that's being highlighted by the mouse or keyboard
-		var highlightedOptionIndex;
-		var highlightedOptionIndexMax = realOptions.length - 1;
-
-		realSelect.setAttribute('data-ss-uuid', uuid);
-		// Even though the element is display: none, a11y users should still see it.
-		// According to http://www.w3.org/TR/wai-aria/states_and_properties#aria-hidden
-		// some browsers may have bugs with this but future implementation may improve
-		realSelect.setAttribute('aria-hidden', "false");
-
-		// Build styled clones of all the real options
-		var selectedOptionHTML;
-		var optionsHTML = '<div class="ss-dropdown">';
-		realOptions.forEach(function(realOption, index){
-			var text = realOption.textContent,
-				value = realOption.getAttribute('value') || '',
-				cssClass = 'ss-option';
-
-			if (index === selectedIndex) {
-				// Mark first item as selected-option - this is where we store state for the styled select box
-				// aria-hidden=true so screen readers ignore the styles selext box in favor of the real one (which is visible by default)
-				selectedOptionHTML = '<div class="ss-selected-option" tabindex="0" data-value="' + value + '">' + text + '</div>'
-			}
-
-			if (realOption.disabled) {
-				cssClass += ' disabled';
-			}
-
-			// Continue building optionsHTML
-			optionsHTML += '<div class="' + cssClass + '" data-value="' + value + '">' + text + '</div>';
-		});
-		optionsHTML += '</div>';
-		styleSelectHTML += selectedOptionHTML += optionsHTML += '</div>';
-
-		// And add out styled select just after the real select
-		realSelect.insertAdjacentHTML('afterend', styleSelectHTML);
-
-		var styledSelect = query('.style-select[data-ss-uuid="'+uuid+'"]');
-
-		// Append the real select to the styled container
-		styledSelect.appendChild(realSelect);
-
-		// Bind onchange event for touch devices only
-		if ( isTouch ) {
-			realSelect.addEventListener('change', function (ev) {
-				var target = ev.target,
-					selectedOption = target.children[target.selectedIndex],
-					newValue = selectedOption.getAttribute('data-value'),
-					newLabel = selectedOption.textContent;
-
-				changeRealSelectBox(newValue, newLabel)
-			});
+		if ( ! realSelects.length ) {
+			return;
 		}
 
-		var styleSelectOptions = styledSelect.querySelectorAll('.ss-option');
-		var selectedOption = styledSelect.querySelector('.ss-selected-option');
+		realSelects.forEach(function (realSelect) {
 
-		var changeRealSelectBox = function(newValue, newLabel) {
-			// Close styledSelect
-			styledSelect.classList.remove('open');
+			var realOptions = realSelect.children,
+				selectedIndex = realSelect.selectedIndex,
+				uuid = makeUUID(),
+				className = 'style-select' + (isTouch ? ' touch' : ''),
+				styleSelectHTML = '<div class="' + className + '" aria-hidden="true" data-ss-uuid="' + uuid + '">';
 
-			// Update styled value
-			selectedOption.textContent = newLabel;
-			selectedOption.dataset.value = newValue;
+			// The index of the item that's being highlighted by the mouse or keyboard
+			var highlightedOptionIndex;
+			var highlightedOptionIndexMax = realOptions.length - 1;
 
-			// Update the 'tick' that shows the option with the current value
-			styleSelectOptions.forEach(function(styleSelectOption){
-				if ( styleSelectOption.dataset.value === newValue) {
-					styleSelectOption.classList.add('ticked')
-				} else {
-					styleSelectOption.classList.remove('ticked')
+			realSelect.setAttribute('data-ss-uuid', uuid);
+			// Even though the element is display: none, a11y users should still see it.
+			// According to http://www.w3.org/TR/wai-aria/states_and_properties#aria-hidden
+			// some browsers may have bugs with this but future implementation may improve
+			realSelect.setAttribute('aria-hidden', "false");
+
+			// Build styled clones of all the real options
+			var selectedOptionHTML;
+			var optionsHTML = '<div class="ss-dropdown">';
+			realOptions.forEach(function(realOption, index){
+				var text = realOption.textContent,
+					value = realOption.getAttribute('value') || '',
+					cssClass = 'ss-option';
+
+				if (index === selectedIndex) {
+					// Mark first item as selected-option - this is where we store state for the styled select box
+					// aria-hidden=true so screen readers ignore the styles selext box in favor of the real one (which is visible by default)
+					selectedOptionHTML = '<div class="ss-selected-option" tabindex="0" data-value="' + value + '">' + text + '</div>'
 				}
-			});
 
-			// Update real select box if not touch only
-			if ( ! isTouch ) {
-				realSelect.value = newValue;
-
-				// Send 'change' event to real select - to trigger any change event listeners
-				var changeEvent = new CustomEvent('change');
-				realSelect.dispatchEvent(changeEvent);
-			}
-		};
-
-		// Change real select box when a styled option is clicked
-		styleSelectOptions.forEach(function(unused, index){
-			var styleSelectOption = styleSelectOptions.item(index);
-
-			if (styleSelectOption.className.match(/\bdisabled\b/)) {
-				return;
-			}
-
-			styleSelectOption.addEventListener('click', function(ev) {
-				var target = ev.target,
-					styledSelectBox = target.parentNode.parentNode,
-					uuid = styledSelectBox.getAttribute('data-ss-uuid'),
-					newValue = target.getAttribute('data-value'),
-					newLabel = target.textContent;
-
-				changeRealSelectBox(newValue, newLabel)
-
-			});
-
-			// Tick and highlight the option that's currently in use
-			if ( styleSelectOption.dataset.value === realSelect.value ) {
-				highlightedOptionIndex = index;
-				styleSelectOption.classList.add('ticked');
-				styleSelectOption.classList.add('highlighted')
-			}
-
-			// Important: we can't use ':hover' as the keyboard and default value can also set the highlight
-			styleSelectOption.addEventListener('mouseover', function(ev){
-				styleSelectOption.parentNode.childNodes.forEach(function(sibling, index){
-					if ( sibling === ev.target ) {
-						sibling.classList.add('highlighted');
-						highlightedOptionIndex = index;
-					} else {
-						sibling.classList.remove('highlighted')
-					}
-				})
-			})
-		});
-
-
-
-		var closeAllStyleSelects = function(exception){
-			queryAll('.style-select').forEach(function(styleSelectEl) {
-				if ( styleSelectEl !== exception ) {
-					styleSelectEl.classList.remove('open');
+				if (realOption.disabled) {
+					cssClass += ' disabled';
 				}
+
+				// Continue building optionsHTML
+				optionsHTML += '<div class="' + cssClass + '" data-value="' + value + '">' + text + '</div>';
 			});
-		};
+			optionsHTML += '</div>';
+			styleSelectHTML += selectedOptionHTML += optionsHTML += '</div>';
 
-		var toggleStyledSelect = function(styledSelectBox){
-			if ( ! isTouch ) {
-				if ( ! styledSelectBox.classList.contains('open') ) {
-					// If we're closed and about to open, close other style selects on the page
-					closeAllStyleSelects(styledSelectBox);
-				}
-				// Then toggle open/close
-				styledSelectBox.classList.toggle('open');
+			// And add out styled select just after the real select
+			realSelect.insertAdjacentHTML('afterend', styleSelectHTML);
+
+			var styledSelect = query('.style-select[data-ss-uuid="'+uuid+'"]');
+
+			// Append the real select to the styled container
+			styledSelect.appendChild(realSelect);
+
+			// Bind onchange event for touch devices only
+			if ( isTouch ) {
+				realSelect.addEventListener('change', function (ev) {
+					var target = ev.target,
+						selectedOption = target.children[target.selectedIndex],
+						newValue = selectedOption.getAttribute('data-value'),
+						newLabel = selectedOption.textContent;
+
+					changeRealSelectBox(realSelect, styledSelect, newValue, newLabel)
+				});
 			}
-		};
 
-		// When a styled select box is clicked
-		var styledSelectedOption = query('.style-select[data-ss-uuid="' + uuid + '"] .ss-selected-option');
-		styledSelectedOption.addEventListener('click', function(ev) {
-			ev.preventDefault();
-			ev.stopPropagation();
-			toggleStyledSelect(ev.target.parentNode);
-		});
+			var styleSelectOptions = styledSelect.querySelectorAll('.ss-option');
 
-		// Keyboard handling
-		styledSelectedOption.addEventListener('keydown', function(ev) {
-			var styledSelectBox = ev.target.parentNode;
+			// Change real select box when a styled option is clicked
+			styleSelectOptions.forEach(function(unused, index){
+				var styleSelectOption = styleSelectOptions.item(index);
 
-			switch (ev.keyCode) {
-				case KEYCODES.SPACE:
-					// Space shows and hides styles select boxes
-					toggleStyledSelect(styledSelectBox);
-					break;
-				case KEYCODES.DOWN:
-				case KEYCODES.UP:
-					// Move the highlight up and down
-					if ( ! styledSelectBox.classList.contains('open') ) {
-						// If style select is not open, up/down should open it.
-						toggleStyledSelect(styledSelectBox);
-					} else {
-						// If style select is already open, these should change what the highlighted option is
-						if ( ev.keyCode === KEYCODES.UP ) {
-							// Up arrow moves earlier in list
-							if ( highlightedOptionIndex !== 0 ) {
-								highlightedOptionIndex = highlightedOptionIndex - 1
-							}
-						} else {
-							// Down arrow moves later in list
-							if ( highlightedOptionIndex < highlightedOptionIndexMax ) {
-								highlightedOptionIndex = highlightedOptionIndex + 1
-							}
-						}
-						styleSelectOptions.forEach(function(option, index){
-							if ( index === highlightedOptionIndex ) {
-								option.classList.add('highlighted')
+				if (styleSelectOption.className.match(/\bdisabled\b/)) {
+					return;
+				}
+
+				styleSelectOption.addEventListener('click', function(ev) {
+					var target = ev.target,
+						uuid = styledSelect.getAttribute('data-ss-uuid'),
+						newValue = target.getAttribute('data-value'),
+						newLabel = target.textContent;
+
+					changeRealSelectBox(realSelect, styledSelect, newValue, newLabel);
+				});
+
+				// Tick and highlight the option that's currently in use
+				if ( styleSelectOption.dataset.value === realSelect.value ) {
+					highlightedOptionIndex = index;
+					styleSelectOption.classList.add('ticked');
+					styleSelectOption.classList.add('highlighted');
+				}
+
+				// Important: we can't use ':hover' as the keyboard and default value can also set the highlight
+				if ( ! isTouch ) {
+					styleSelectOption.addEventListener('mouseover', function(ev){
+						styleSelectOption.parentNode.childNodes.forEach(function(sibling, index){
+							if ( sibling === ev.target ) {
+								sibling.classList.add('highlighted');
+								highlightedOptionIndex = index;
 							} else {
-								option.classList.remove('highlighted')
+								sibling.classList.remove('highlighted');
 							}
-						})
-					}
-					ev.preventDefault();
-					ev.stopPropagation();
-					break;
-				// User has picked an item from the keyboard
-				case KEYCODES.ENTER:
-					var highlightedOption = styledSelectedOption.parentNode.querySelectorAll('.ss-option')[highlightedOptionIndex],
-						newValue = highlightedOption.dataset.value,
-						newLabel = highlightedOption.textContent;
+						});
+					});
+				}
+			});
 
-					if ( ! highlightedOption.classList.contains('disabled') ) {
-						changeRealSelectBox(newValue, newLabel);
-					}
-					ev.preventDefault();
-					ev.stopPropagation();
-					break;
-			}
+			// When a styled select box is clicked
+			var styledSelectedOption = query('.style-select[data-ss-uuid="' + uuid + '"] .ss-selected-option');
+
+			styledSelectedOption.addEventListener('click', function(ev) {
+				ev.preventDefault();
+				ev.stopPropagation();
+				toggleStyledSelect(ev.target.parentNode);
+			});
+
+			// Keyboard handling
+			styledSelectedOption.addEventListener('keydown', function(ev) {
+				switch (ev.keyCode) {
+					case KEYCODES.SPACE:
+						// Space shows and hides styles select boxes
+						toggleStyledSelect(styledSelect);
+						break;
+					case KEYCODES.DOWN:
+					case KEYCODES.UP:
+						// Move the highlight up and down
+						if ( ! styledSelect.classList.contains('open') ) {
+							// If style select is not open, up/down should open it.
+							toggleStyledSelect(styledSelect);
+						} else {
+							// If style select is already open, these should change what the highlighted option is
+							if ( ev.keyCode === KEYCODES.UP ) {
+								// Up arrow moves earlier in list
+								if ( highlightedOptionIndex !== 0 ) {
+									highlightedOptionIndex = highlightedOptionIndex - 1
+								}
+							} else {
+								// Down arrow moves later in list
+								if ( highlightedOptionIndex < highlightedOptionIndexMax ) {
+									highlightedOptionIndex = highlightedOptionIndex + 1
+								}
+							}
+							styleSelectOptions.forEach(function(option, index){
+								if ( index === highlightedOptionIndex ) {
+									option.classList.add('highlighted')
+								} else {
+									option.classList.remove('highlighted')
+								}
+							})
+						}
+						ev.preventDefault();
+						ev.stopPropagation();
+						break;
+					// User has picked an item from the keyboard
+					case KEYCODES.ENTER:
+						var highlightedOption = styledSelectedOption.parentNode.querySelectorAll('.ss-option')[highlightedOptionIndex],
+							newValue = highlightedOption.dataset.value,
+							newLabel = highlightedOption.textContent;
+
+						if ( ! highlightedOption.classList.contains('disabled') ) {
+							changeRealSelectBox(realSelect, styledSelect, newValue, newLabel);
+						}
+						ev.preventDefault();
+						ev.stopPropagation();
+						break;
+				}
+			});
 		});
 
 		// Clicking outside of the styled select box closes any open styled select boxes
-		query('body').addEventListener('click', function(ev){
-
+		query('html').addEventListener('click', function(ev){
 			if ( ! isAncestorOf(ev.target, '.style-select', true) ) {
 				closeAllStyleSelects();
 			}
-		})
+		});
+
+		return realSelects.length > 1 ? realSelects : realSelects[0];
 
 	};
 
